d5fed4a9a7fef25f349e55cc5593801b4a1e1872
felixnjaala <felix.njaala03.com>
2026-05-01 10:20:40 +0300

    Define Weather entity with properties for id, city, temperature, and description, integrating TypeORM for database management.

--- src/weather/entities/weather.entity.ts
+++ src/weather/entities/weather.entity.ts
@@ -1,1 +1,16 @@
-export class Weather {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Weather {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  city: string;
+
+  @Column('float')
+  temperature: number;
+
+  @Column()
+  description: string;
+}
