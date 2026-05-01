0582f2b76d8c77d219ad772b07aa29bc37b0467b
felixnjaala <felix.njaala03.com>
2026-05-01 10:20:14 +0300

    Integrate HttpModule and TypeOrmModule in WeatherModule for enhanced API interactions and data management

--- src/weather/weather.module.ts
+++ src/weather/weather.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { WeatherService } from './weather.service';
 import { WeatherController } from './weather.controller';
+import { Weather } from './entities/weather.entity';
 
 @Module({
+  imports: [HttpModule, TypeOrmModule.forFeature([Weather])],
   controllers: [WeatherController],
   providers: [WeatherService],
 })
