6acbc1be24509cbc2511e3ca4959aafaad50577a
felixnjaala <felix.njaala03.com>
2026-05-01 10:20:04 +0300

    Refactor WeatherController to rename create method to fetchWeather and remove unused endpoints for improved clarity and functionality

--- src/weather/weather.controller.ts
+++ src/weather/weather.controller.ts
@@ -1,34 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
 import { WeatherService } from './weather.service';
 import { CreateWeatherDto } from './dto/create-weather.dto';
-import { UpdateWeatherDto } from './dto/update-weather.dto';
 
 @Controller('weather')
 export class WeatherController {
-  constructor(private readonly weatherService: WeatherService) {}
+  constructor(private readonly weatherService: WeatherService) { }
 
   @Post()
-  create(@Body() createWeatherDto: CreateWeatherDto) {
-    return this.weatherService.create(createWeatherDto);
+  fetchWeather(@Body() dto: CreateWeatherDto) {
+    return this.weatherService.fetchWeather(dto);
   }
 
   @Get()
   findAll() {
     return this.weatherService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.weatherService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateWeatherDto: UpdateWeatherDto) {
-    return this.weatherService.update(+id, updateWeatherDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.weatherService.remove(+id);
   }
 }
