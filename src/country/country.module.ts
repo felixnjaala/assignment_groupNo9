0444a9e697994ce6d1d1ff89a712ffcb3b76aae3
felixnjaala <felix.njaala03@gmail.com>
2026-05-01 10:19:37 +0300

    Enhance CountryModule by integrating HttpModule and TypeOrmModule for improved data handling and API interactions

--- src/country/country.module.ts
+++ src/country/country.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CountryService } from './country.service';
 import { CountryController } from './country.controller';
+import { Country } from './entities/country.entity';
 
 @Module({
+  imports: [HttpModule, TypeOrmModule.forFeature([Country])],
   controllers: [CountryController],
   providers: [CountryService],
 })
