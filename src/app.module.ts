56c85995fe3b0ca0c238adb79df1609e4d45f857
felixnjaala <felix.njaala03@gmail.com>
2026-05-01 10:19:22 +0300

    Integrate TypeORM with PostgreSQL in AppModule for database connectivity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { WeatherModule } from './weather/weather.module';
 import { CountryModule } from './country/country.module';
 
 @Module({
-  imports: [WeatherModule, CountryModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST ?? 'localhost',
+      port: Number(process.env.DB_PORT ?? 5432),
+      username: process.env.DB_USERNAME ?? 'postgres',
+      password: process.env.DB_PASSWORD ?? '',
+      database: process.env.DB_NAME ?? 'assignment',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    WeatherModule,
+    CountryModule,
+  ],
 })
-export class AppModule {}
+export class AppModule { }
