f21b9008e3ab5cfa1b1e44800a2de750cd2609d4
felixnjaala <felix.njaala03.com>
2026-05-01 10:19:44 +0300

    Implement country fetching functionality in CountryService, utilizing HttpService and TypeORM for data persistence

--- src/country/country.service.ts
+++ src/country/country.service.ts
@@ -1,26 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Country } from './entities/country.entity';
 import { CreateCountryDto } from './dto/create-country.dto';
-import { UpdateCountryDto } from './dto/update-country.dto';
 
 @Injectable()
 export class CountryService {
-  create(createCountryDto: CreateCountryDto) {
-    return 'This action adds a new country';
+  constructor(
+    private readonly httpService: HttpService,
+    @InjectRepository(Country)
+    private readonly countryRepo: Repository<Country>,
+  ) {}
+
+  async fetchCountry(dto: CreateCountryDto) {
+    const url = `https://restcountries.com/v3.1/name/${dto.name}`;
+
+    try {
+      const response = await firstValueFrom(this.httpService.get(url));
+      const data = response.data[0];
+
+      const country = this.countryRepo.create({
+        name: data.name.common,
+        capital: data.capital ? data.capital[0] : '',
+        region: data.region,
+        population: data.population,
+      });
+
+      return await this.countryRepo.save(country);
+    } catch (error: unknown) {
+      const message = error instanceof Error ? error.message : 'Unknown error';
+      throw new Error(`Country API failed: ${message}`);
+    }
   }
 
-  findAll() {
-    return `This action returns all country`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} country`;
-  }
-
-  update(id: number, updateCountryDto: UpdateCountryDto) {
-    return `This action updates a #${id} country`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} country`;
+  async findAll() {
+    return this.countryRepo.find();
   }
 }
